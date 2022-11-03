80d127b2a08daab44e8191968305df5925270b72
Andra Helton <andrahelton@gmail.com>
2022-11-02 19:46:46 -0600

    Initial commit

---
+++ friends.rb
@@ -0,0 +1,8 @@
+friends = ["James", "Isaac", "Andra", "Conner", "Kaitlyn"]
+friends.each do |friend|
+    if friend == "James"
+        puts "Hang in there #{friend}!"
+    else 
+        puts "You got it #{friend}!"
+    end
+end
